b192b546a5a852542950807d950a704a7340e946
Lukas Elmer <lukas.elmer@gmail.com>
2019-10-18 13:05:19 +0300

    Add images

--- src/components/timeline-element.tsx
+++ src/components/timeline-element.tsx
@@ -13,95 +13,125 @@ export function TimelineElement(props: P) {
 
   const entry = props.entry
   return (
-    <article
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        // alignItems: 'stretch',
-        alignContent: 'center',
-        // justifyContent: 'center',
-        padding: '10px',
-        visibility: visible ? 'visible' : 'hidden',
-      }}
+    <VisibilitySensor
+      partialVisibility={true}
+      offset={{ bottom: 40 }}
+      onChange={isVisible => isVisible && setVisible(true)}
     >
-      <section
+      <article
         style={{
-          zIndex: 2,
-          flexShrink: 0,
-          flexGrow: 0,
-          position: 'relative',
-          animation: visible ? 'bounce-scale 0.6s' : '',
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          // alignItems: 'stretch',
+          alignContent: 'center',
+          // justifyContent: 'center',
+          padding: '10px',
+          visibility: visible ? 'visible' : 'hidden',
         }}
       >
-        <div
+        <section
           style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignContent: 'center',
-            background: color(entry.type),
-            color: '#fff',
-            border: '5px solid transparent',
-            borderColor: leftTimelineColor,
-            borderRadius: '50%',
-            width: '95px',
-            height: '95px',
+            zIndex: 2,
+            flexShrink: 0,
+            flexGrow: 0,
+            position: 'relative',
+            animation: visible ? 'bounce-scale 0.6s' : '',
           }}
         >
-          <div style={{ alignSelf: 'center' }}>
-            <VisibilitySensor
-              partialVisibility={true}
-              offset={{ bottom: 40 }}
-              onChange={isVisible => isVisible && setVisible(true)}
-            >
-              {icon(entry.type)}
-            </VisibilitySensor>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignContent: 'center',
+              background: color(entry.type),
+              color: '#fff',
+              border: '5px solid transparent',
+              borderColor: leftTimelineColor,
+              borderRadius: '50%',
+              width: '95px',
+              height: '95px',
+            }}
+          >
+            <div style={{ alignSelf: 'center' }}>{icon(entry.type)}</div>
           </div>
-        </div>
-      </section>
-      <section style={{ zIndex: 2, flexShrink: 0, flexGrow: 0, position: 'relative' }}>
-        <div
+        </section>
+        <section style={{ zIndex: 2, flexShrink: 0, flexGrow: 0, position: 'relative' }}>
+          <div
+            style={{
+              display: 'flex',
+              borderWidth: '15px',
+              borderStyle: 'solid',
+              borderColor: 'transparent',
+              borderRightColor: color(entry.type),
+              width: '0px',
+              marginLeft: '5px',
+              height: '0px',
+              marginRight: '-2px',
+              animation: visible ? 'bounce-left-to-right 0.6s' : '',
+            }}
+          />
+        </section>
+        <section
           style={{
-            display: 'flex',
-            borderWidth: '15px',
-            borderStyle: 'solid',
-            borderColor: 'transparent',
-            borderRightColor: color(entry.type),
-            width: '0px',
-            marginLeft: '5px',
-            height: '0px',
-            marginRight: '-2px',
+            background: color(entry.type),
+            color: '#fff',
+            borderRadius: '10px',
+            padding: '5px 20px',
+            flexGrow: 1,
             animation: visible ? 'bounce-left-to-right 0.6s' : '',
+            flexBasis: 1,
           }}
-        />
-      </section>
-      <section
-        style={{
-          background: color(entry.type),
-          color: '#fff',
-          borderRadius: '10px',
-          padding: '5px 20px',
-          flexGrow: 1,
-          animation: visible ? 'bounce-left-to-right 0.6s' : '',
-        }}
-      >
-        <h3 className="vertical-timeline-element-title">{entry.title}</h3>
-        <h4 className="vertical-timeline-element-subtitle">
-          {entry.date.toLocaleString()} - {entry.type}
-        </h4>
-        <p>{entry.desc}</p>
-        {entry.result && (
+        >
+          <h3 className="vertical-timeline-element-title">{entry.title}</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            {entry.date.toLocaleString()} - {entry.type}
+          </h4>
+          <p>{entry.desc}</p>
+          {entry.result && (
+            <p>
+              1st moto: {entry.result.first}, 2nd moto: {entry.result.second}, Overall:{' '}
+              {entry.result.overall}
+            </p>
+          )}
           <p>
-            1st moto: {entry.result.first}, 2nd moto: {entry.result.second}, Overall:{' '}
-            {entry.result.overall}
+            <a href={entry.link} target="_blank">
+              Details
+            </a>
           </p>
+        </section>
+        {entry.images.length > 0 && (
+          <section
+            style={{
+              flexGrow: 1,
+              animation: visible ? 'bounce-left-to-right 0.6s' : '',
+              flexBasis: 1,
+            }}
+          >
+            {entry.images.map(image => {
+              return (
+                <img
+                  key={image}
+                  src={image}
+                  style={{
+                    maxWidth: imagesWidth(entry.images.length),
+                    borderRadius: '10px',
+                    background: color(entry.type),
+                    padding: '5px',
+                    margin: '5px',
+                  }}
+                />
+              )
+            })}
+          </section>
         )}
-        <p>
-          <a href={entry.link} target="_blank">
-            Details
-          </a>
-        </p>
-      </section>
-    </article>
+      </article>
+    </VisibilitySensor>
   )
+}
+
+function imagesWidth(imageCount: number) {
+  if (imageCount === 1) return '100%'
+  if (imageCount === 2) return '45%'
+  return '25%'
 }
--- src/data/timeline-entries.ts
+++ src/data/timeline-entries.ts
@@ -2,14 +2,17 @@ import { parseISO } from 'date-fns'
 import { Unpacked } from '../utils'
 import { timeline2019 } from './data-2019'
 
-export const timeline = timeline2019.map(entry => ({
-  type: entry.type,
-  title: entry.title as string,
-  desc: entry.desc as string,
-  date: parseISO(entry.date),
-  link: 'link' in entry ? entry.link : '',
-  result: 'result' in entry ? parseResult(entry.result) : null,
-}))
+export const timeline = Object.freeze(
+  timeline2019.map(entry => ({
+    type: entry.type,
+    title: entry.title as string,
+    desc: entry.desc as string,
+    date: parseISO(entry.date),
+    link: 'link' in entry ? entry.link : '',
+    result: 'result' in entry ? parseResult(entry.result) : null,
+    images: 'images' in entry ? entry.images : ([] as readonly string[]),
+  }))
+)
 
 function parseResult(result: string) {
   const all = result.split('-').map(str => parseInt(str, 10))
